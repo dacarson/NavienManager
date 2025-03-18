a5898b686f7d9ff83db266fc884937262d056cec
dacarson <44933987+dacarson@users.noreply.github.com>
2025-03-18 15:51:50 -0700

    Split FakeGatoHomeSpan.ino into header+implementation
    To make things simpler, split the ino file into a header and an implementation file. Rename the header and implementation to match class name.

---
+++ FakeGatoHistoryService.cpp
@@ -0,0 +1,284 @@
+/*
+ Copyright (c) 2025 David Carson (dacarson)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Based on Eve History Protocol documented here:
+ https://github.com/An00bIS47/Homekit/blob/3908e0799cc9fae23791c562420145f8f8491269/docs/eve/Elgato-Eve-History-Protocol.md
+ and Eve History implementation here:
+ https://github.com/simont77/fakegato-history
+ */
+
+#define CUSTOM_CHAR_HEADER
+#include "FakeGatoHistoryService.h"
+
+#define LOG_ENTRY_FREQ_TEN_MIN 600000
+#define LOG_ENTRY_FREQ_ONE_MIN  60000
+#define HISTORY_FILE "/history.bin"
+
+FakeGatoHistoryService::FakeGatoHistoryService() 
+  : Service::FakeGatoHistoryData() {
+    Serial.println(F("Configuring Eve History Service"));
+
+    memset(&historyStatusData, 0, sizeof(HistoryStatusData));
+    historyStatusData.status.paramCount = 0x5;
+    uint8_t tempSignature[10] = {0x01, 0x02, 0x11, 0x02, 0x10, 0x01, 0x12, 0x01, 0x1D, 0x01};
+    memcpy(historyStatusData.status.signature, tempSignature, sizeof(tempSignature));
+
+    if (loadHistory()) {
+        Serial.println("Restored History from file");
+    } else {
+        Serial.println("Failed to restore History from file, using empty history.");
+    }
+
+    updateAndSetHistoryStatus();
+    historyEntries.setData(0, 0);
+}
+
+void FakeGatoHistoryService::accumulateLogEntry(float currentTemp, float targetTemp, uint8_t valvePercent, uint8_t thermoTarget, uint8_t openWindow) {
+    if (!currentTemp || !targetTemp) return;
+
+    bool keyValueChanged = ((uint16_t)(targetTemp * 100) != store.history[store.lastEntry % store.historySize].targetTemp ||
+                            thermoTarget != store.history[store.lastEntry % store.historySize].thermoTarget ||
+                            valvePercent > 0);
+
+    if (keyValueChanged && logInterval == LOG_ENTRY_FREQ_TEN_MIN) {
+        if (avgLog.count > 0) generateTimedHistoryEntry();
+        logInterval = LOG_ENTRY_FREQ_ONE_MIN;
+    } else if (valvePercent == 0 && logInterval == LOG_ENTRY_FREQ_ONE_MIN) {
+        if (avgLog.count > 0) generateTimedHistoryEntry();
+        logInterval = LOG_ENTRY_FREQ_TEN_MIN;
+    }
+
+    avgLog.totalTemp += currentTemp;
+    avgLog.totalTargetTemp += targetTemp;
+    avgLog.totalValvePos += valvePercent;
+    avgLog.lastThermoTarget = thermoTarget;
+    avgLog.lastOpenWindow = openWindow;
+    avgLog.count++;
+}
+
+void FakeGatoHistoryService::generateTimedHistoryEntry() {
+    if (avgLog.count > 0) {
+        float avgTemp = avgLog.totalTemp / avgLog.count;
+        float avgTargetTemp = avgLog.totalTargetTemp / avgLog.count;
+        uint8_t avgValvePos = avgLog.totalValvePos / avgLog.count;
+        addHistoryEntry(avgTemp, avgTargetTemp, avgValvePos, avgLog.lastThermoTarget, avgLog.lastOpenWindow);
+    }
+    avgLog = {0};
+    updateAndSetHistoryStatus();
+}
+
+void FakeGatoHistoryService::addHistoryEntry(float currentTemp, float targetTemp, uint8_t valvePercent, uint8_t thermoTarget, uint8_t openWindow) {
+    if (store.usedMemory < store.historySize) {
+        store.usedMemory++;
+        store.firstEntry = 0;
+        store.lastEntry = store.usedMemory;
+    } else {
+        store.firstEntry++;
+        store.lastEntry = store.firstEntry + store.usedMemory;
+    }
+
+    store.history[store.lastEntry % store.historySize].time = time(nullptr);
+    store.history[store.lastEntry % store.historySize].currentTemp = (uint16_t)(currentTemp * 100);
+    store.history[store.lastEntry % store.historySize].targetTemp = (uint16_t)(targetTemp * 100);
+    store.history[store.lastEntry % store.historySize].valvePercent = valvePercent;
+    store.history[store.lastEntry % store.historySize].thermoTarget = thermoTarget;
+    store.history[store.lastEntry % store.historySize].openWindow = openWindow;
+
+    saveHistory();
+}
+
+void FakeGatoHistoryService::printData(uint8_t *data, int len) {
+  Serial.printf(F("Data %d "), len);
+  for (int i = 0; i < len; i++) {
+    if (data[i] < 0x10)
+        Serial.printf(F("0%x "), data[i]);
+    else
+      Serial.printf(F("%x "), data[i]);
+  }
+  Serial.println("");
+}
+
+bool FakeGatoHistoryService::saveHistory() {
+    File file = LittleFS.open(HISTORY_FILE, "w");
+    if (!file) return false;
+    size_t written = file.write((uint8_t*)&store, sizeof(PersistHistoryData));
+    file.close();
+    return written == sizeof(PersistHistoryData);
+}
+
+bool FakeGatoHistoryService::loadHistory() {
+    File file = LittleFS.open(HISTORY_FILE, "r");
+    if (!file) return false;
+    size_t readBytes = file.read((uint8_t*)&store, sizeof(PersistHistoryData));
+    file.close();
+    return readBytes == sizeof(PersistHistoryData);
+}
+
+void FakeGatoHistoryService::eraseHistory() {
+      memset((uint8_t*)&store, 0, sizeof(PersistHistoryData));
+      saveHistory();
+      Serial.println("History data erased");
+  }
+
+  void FakeGatoHistoryService::sendHistory(uint32_t currentEntry) {
+    if (currentEntry == 0)
+        currentEntry = 1;
+    
+    uint8_t historySendBuffer[960];  // Max HomeKit response size
+    int sendBufferLen = 0;
+    
+    //WEBLOG("Send History currentEntry %d store.lastEntry %d\n", currentEntry, store.lastEntry);
+    if (currentEntry <= store.lastEntry) {
+      uint32_t memoryAddress = currentEntry % store.historySize;
+      
+      // Build up the history send buffer
+      for (int i = 0; i < 11; i++) {
+        Serial.printf(F("MemoryAddress %d\n"), memoryAddress);
+        if ((store.history[memoryAddress].time == 0) || (sendTime) || currentEntry == store.firstEntry + 1) { // Its a store.refTime entry or we need to set the time
+          Serial.println(F("Sending special Ref Time history entry"));
+          uint8_t refEntry[21];
+          memset(refEntry, 0, 21);
+          refEntry[0] = 21; // entry length
+          memcpy(&refEntry[1], &currentEntry, sizeof(uint32_t)); // Current Entry
+          refEntry[5] = 0x1; // Secs since reference time set
+          refEntry[9] = 0x81;
+          memcpy(&refEntry[10], &store.refTime, sizeof(uint32_t));
+          
+          sendTime = false;
+          memcpy(&historySendBuffer[sendBufferLen], &refEntry, 21);
+          sendBufferLen += 21;
+        } else {
+          uint8_t dataEntry[17];
+          dataEntry[0] = 17; // entry length
+          memcpy(&dataEntry[1], &currentEntry, sizeof(uint32_t));
+          // Don't allow negative offsets as the variable is unsigned.
+          uint32_t refOffset = 0;
+          if ((store.history[memoryAddress].time - EPOCH_OFFSET) >=  store.refTime) {
+            refOffset = (store.history[memoryAddress].time - EPOCH_OFFSET) - store.refTime;
+          }
+          memcpy(&dataEntry[5], &refOffset, sizeof(uint32_t));
+          dataEntry[9] = 0x1f;
+          memcpy(&dataEntry[10], &store.history[memoryAddress].currentTemp, sizeof(uint16_t));
+          memcpy(&dataEntry[12], &store.history[memoryAddress].targetTemp, sizeof(uint16_t));
+          dataEntry[14] = store.history[memoryAddress].valvePercent;
+          dataEntry[15] = store.history[memoryAddress].thermoTarget;
+          dataEntry[16] = store.history[memoryAddress].openWindow;
+          memcpy(&historySendBuffer[sendBufferLen], &dataEntry, 17);
+          sendBufferLen += 17;
+        }
+        currentEntry++;
+        memoryAddress = currentEntry % store.historySize;
+        if (currentEntry > store.lastEntry)
+            break;
+      }
+      Serial.println(F("Sending History"));
+      printData(historySendBuffer, sendBufferLen);
+      historyEntries.setData(historySendBuffer, sendBufferLen);
+    } else {
+      Serial.println(F("No History To Send"));
+      historyEntries.setData(0, 0);
+    }
+  }
+
+void FakeGatoHistoryService::updateAndSetHistoryStatus() {
+    historyStatusData.status.timeSinceLastUpdate = (time(nullptr) - EPOCH_OFFSET) - store.refTime;
+    historyStatusData.status.refTime = store.refTime;
+    historyStatus.setData(historyStatusData.raw_data, sizeof(historyStatusData.raw_data), false);
+}
+
+void FakeGatoHistoryService::loop() {
+    if (historyStatus.timeVal() >= logInterval) generateTimedHistoryEntry();
+}
+
+boolean FakeGatoHistoryService::update() {
+    
+    if (historyRequest.updated()) {
+      int len = historyRequest.getNewData(0, 0);
+      uint8_t *data = (uint8_t *)malloc(sizeof(uint8_t) * len);
+      historyRequest.getNewData(data, len);
+      uint32_t address = *(uint32_t*)&data[2];
+      
+      Serial.printf(F("History Request %d\n"), address);
+      sendHistory(address);
+      delete data;
+    }
+    
+    if (setTime.updated()) {
+      int len = setTime.getNewData(0, 0);
+      uint8_t *data = (uint8_t *)malloc(sizeof(uint8_t) * len);
+      setTime.getNewData(data, len);
+      Serial.print(F("Set Time "));
+      uint32_t eveTimestamp = *(uint32_t*)data;
+      
+      time_t currentTime = eveTimestamp + EPOCH_OFFSET;  // Convert to Unix timestamp (since 1970)
+      
+      struct tm *timeInfo = localtime(&currentTime);          // Convert to time structure
+      Serial.printf(F("%02d:%02d %02d/%02d/%04d\n"),
+              timeInfo->tm_hour, timeInfo->tm_min,
+              timeInfo->tm_mon + 1, timeInfo->tm_mday,  // tm_mon is 0-based
+              timeInfo->tm_year + 1900);               // tm_year is years since 1900
+      
+      uint32_t before = time(nullptr);
+
+      Serial.print(F("Checking the clock. Before: "));
+      Serial.print(before);
+      Serial.print(F(", After: "));
+      Serial.print(currentTime);
+      Serial.print(F(", Elapsed: "));
+      Serial.println(currentTime - before);
+      
+      if (currentTime - before > 5) {
+        Serial.println(F("Updating local clock"));
+        
+        // set the clock
+        struct timeval tv;
+        tv.tv_sec = currentTime;
+        tv.tv_usec = 0;
+        settimeofday(&tv, NULL);
+
+        // Check to see if the reference time was set, but the clock had not been set yet.
+        // If so, then we need to correct it
+        if (store.refTime != 0 && store.refTime > currentTime ) {
+          Serial.printf(F("Fixing refTime %u to %u\n"), store.refTime, eveTimestamp);
+          store.refTime = eveTimestamp - (store.refTime + EPOCH_OFFSET);
+          Serial.printf(F("Fixed refTime %u\n"), store.refTime);
+
+          // Fix up the History Status Data and republish.
+          historyStatusData.status.timeSinceLastUpdate = (currentTime - EPOCH_OFFSET) - store.refTime;
+          Serial.printf(F("Correcting timeSincceLastUpdate %u\n"), historyStatusData.status.timeSinceLastUpdate);
+          historyStatusData.status.refTime = store.refTime;
+          historyStatus.setData(historyStatusData.raw_data, sizeof(historyStatusData.raw_data));
+
+          // Fix up history entries
+          for (int i = 1; i <= store.lastEntry; i++) {
+            if (store.history[i].time != 0) { // Skip store.refTime 0x81 history entries
+              //Serial.printf("Fixing history entry %d from %u to %u\n", i, store.history[i].time, store.history[i].time + currentTime - before);
+              store.history[i].time += (currentTime - before);
+            }
+          }
+        }
+      }
+      
+      delete data;
+    }
+    
+    return true;
+  }
---
+++ FakeGatoHistoryService.h
@@ -0,0 +1,107 @@
+/*
+ Copyright (c) 2025 David Carson (dacarson)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Based on Eve History Protocol documented here:
+ https://github.com/An00bIS47/Homekit/blob/3908e0799cc9fae23791c562420145f8f8491269/docs/eve/Elgato-Eve-History-Protocol.md
+ and Eve History implementation here:
+ https://github.com/simont77/fakegato-history
+ */
+
+#pragma once
+
+#include "HomeSpan.h"
+#include <LittleFS.h>
+
+#define EPOCH_OFFSET 978307200
+#define MEMORY_SIZE 3024
+
+CUSTOM_SERV(FakeGatoHistoryData, E863F007-079E-48FF-8F27-9C2605A29F52);
+CUSTOM_CHAR_DATA(EveHistoryStatus, E863F116-079E-48FF-8F27-9C2605A29F52, PR+EV+HD);
+CUSTOM_CHAR_DATA(EveHistoryEntries, E863F117-079E-48FF-8F27-9C2605A29F52, PR+EV+HD);
+CUSTOM_CHAR_DATA(EveHistoryRequest, E863F11C-079E-48FF-8F27-9C2605A29F52, PW+EV+HD);
+CUSTOM_CHAR_DATA(EveSetTime, E863F121-079E-48FF-8F27-9C2605A29F52, PW+EV+HD);
+
+struct LogEntry {
+    uint32_t time;
+    uint16_t currentTemp;
+    uint16_t targetTemp;
+    uint8_t valvePercent;
+    uint8_t thermoTarget;
+    uint8_t openWindow;
+};
+
+union HistoryStatusData {
+    struct {
+        uint32_t timeSinceLastUpdate;
+        uint32_t negativeOffsetrefTime;
+        uint32_t refTime;
+        uint8_t paramCount;
+        uint8_t signature[10];
+    } status;
+    uint8_t raw_data[38];
+};
+
+struct PersistHistoryData {
+    uint16_t historySize = MEMORY_SIZE;
+    LogEntry history[MEMORY_SIZE];
+    uint16_t firstEntry = 0;
+    uint16_t lastEntry = 0;
+    uint16_t usedMemory = 0;
+    uint32_t refTime = 0;
+};
+
+struct AveragedEntry {
+    uint32_t count;
+    float totalTemp;
+    float totalTargetTemp;
+    uint16_t totalValvePos;
+    uint8_t lastThermoTarget;
+    uint8_t lastOpenWindow;
+};
+
+class FakeGatoHistoryService : public Service::FakeGatoHistoryData {
+public:
+    FakeGatoHistoryService();
+    void accumulateLogEntry(float currentTemp, float targetTemp, uint8_t valvePercent, uint8_t thermoTarget, uint8_t openWindow);
+    void generateTimedHistoryEntry();
+    void addHistoryEntry(float currentTemp, float targetTemp, uint8_t valvePercent, uint8_t thermoTarget, uint8_t openWindow);
+    bool saveHistory();
+    bool loadHistory();
+    void eraseHistory();
+    void updateAndSetHistoryStatus();
+    void sendHistory(uint32_t currentEntry);
+    boolean update() override;
+    void loop() override;
+
+    PersistHistoryData store;
+
+private:
+    Characteristic::EveHistoryStatus historyStatus;
+    Characteristic::EveHistoryEntries historyEntries;
+    Characteristic::EveHistoryRequest historyRequest;
+    Characteristic::EveSetTime setTime;
+    HistoryStatusData historyStatusData;
+    AveragedEntry avgLog = {0};
+    bool restarted = true;
+    bool sendTime = true;
+    int logInterval;
+    void printData(uint8_t *data, int len);
+};
